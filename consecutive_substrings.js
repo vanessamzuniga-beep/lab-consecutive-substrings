f8e269c8256737d7b5422f98d5e4d5131ac71f54
vanessamzuniga-beep <vanessamzuniga@gmail.com>
2026-06-19 10:33:45 -0500

    Lab

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,6 +1,21 @@
 function consecutiveSubstrings(string) {
   // type your code here
+  const result = []
+
+  // If string is empty, return[]
+  if(string.length === 0) return result
+
+  // Loop through each starting point (anchor)
+  for(let i = 0; i < string.length; i++) {
+
+    // loop through each possible ending points
+    for(let j = i + 1; j <= string.length; j++) {
+      result.push(string.slice(i, j))
+    }
+  }
+  return result
 }
+
 
 if (require.main === module) {
   // add your own tests in here
